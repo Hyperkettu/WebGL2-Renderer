8ad0f4dcfc81cc2f580aa1fab16f3f46558b7ca4
Olli Kettunen <oj.kettunen@gmail.com>
2020-07-17 14:03:16 +0300

    PBR fix

--- src/shaders/pbr-common.ts
+++ src/shaders/pbr-common.ts
@@ -209,7 +209,7 @@ vec4 ogl_pbr(vec3 N, vec3 albedo, float roughness, float metallic, float ao) {
 
 	// sample both the pre-filter map and the BRDF lut and combine them together as per the Split-Sum approximation to get the IBL specular part.
 	const float MAX_REFLECTION_LOD = 5.0f;
-	vec3 prefilteredColor = EnvRemap(textureLod(prefilterMap, R, roughness * MAX_REFLECTION_LOD).rgb);
+	vec3 prefilteredColor = textureLod(prefilterMap, R, roughness * MAX_REFLECTION_LOD).rgb;
 	vec2 brdf = texture(brdfLUT, vec2(max(dot(N, V), 0.0f), roughness)).rg;
 	vec3 specular = prefilteredColor * (F * brdf.x + brdf.y);
 
