d7c71c9e4b68e80e934ce5a04a9b7942da067918
Olli Kettunen <oj.kettunen@gmail.com>
2020-07-24 19:16:06 +0300

    Flip y axis for text textures

--- src/texttexture.ts
+++ src/texttexture.ts
@@ -67,14 +67,14 @@ export class TextTexture extends Texture {
     }
     
     private generateWebGLTexture(gl: WebGL2RenderingContext, canvas: HTMLCanvasElement) {
-        gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+    //    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
         gl.bindTexture(gl.TEXTURE_2D, this.textureId);
         gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, canvas); 
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_NEAREST);
         gl.generateMipmap(gl.TEXTURE_2D);
         gl.bindTexture(gl.TEXTURE_2D, null);
-        gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, false);
+    //    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, false);
 
     }
 
